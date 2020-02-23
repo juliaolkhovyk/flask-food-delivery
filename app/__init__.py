093b596a4db7c81e84d62bfc404f2997fb9cc09b
julia <olkhovyk_ak16@nuwm.edu.ua>
2020-02-24 00:02:19 +0200

    templates

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_login import LoginManager
-from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 
 db = SQLAlchemy()
@@ -23,11 +22,6 @@ migrate.init_app(app, db)
 
 from app.models import User, Order, Category, Meal
 from app import views
+from app.admin import admin
 
-admin = Admin(app)
-admin.add_view(ModelView(Order, db.session))
-admin.add_view(ModelView(Meal, db.session))
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Category, db.session))
-#admin.add_view(ModelView(orders_meals_association, db.session))
 
--- app/admin.py
+++ app/admin.py
@@ -1,2 +1,31 @@
-from app import admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user
+from flask_admin import Admin, AdminIndexView
+from app import app
+from app.models import Order, Meal, User, Category, Association
+from app import db
+from flask import redirect, url_for
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('login'))
+            
+class MyAdminIndexView(AdminIndexView):
+        def is_accessible(self):
+            if current_user.is_authenticated:
+                return current_user.is_admin
+
+        def inaccessible_callback(self, name, **kwargs):
+            return redirect(url_for('login'))
+        
+
+admin = Admin(app, index_view=MyAdminIndexView())
+admin.add_view(ModelView(Order, db.session))
+admin.add_view(ModelView(Meal, db.session))
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Category, db.session))
+admin.add_view(ModelView(Association, db.session))
+
